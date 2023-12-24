34d0e7f20745f4ebeb4ddd1dbcdc8bbeb2522d8b
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-24 19:10:15 +0530

    refactor: experimenting with regex to get accurate details

--- backend-vision/index.js
+++ backend-vision/index.js
@@ -1,20 +1,3 @@
-// const vision = require("@google-cloud/vision");
-
-// const client = new vision.ImageAnnotatorClient({
-//   keyFilename: "../../APIKey.json",
-// });
-
-// client
-//   .textDetection("./abc.jpg")
-//   .then((results) => {
-//     // const text = results.fullTextAnnotation;
-
-//     console.log(results[0].fullTextAnnotation.text);
-//   })
-//   .catch((err) => {
-//     console.error("ERROR:", err);
-//   });
-
 const vision = require("@google-cloud/vision");
 
 const client = new vision.ImageAnnotatorClient({
@@ -26,37 +9,37 @@ client
   .then((results) => {
     const text = results[0].fullTextAnnotation.text;
 
-    // Extract information using refined regular expressions
-    const idRegex = /\d{1,4}\s?\d{5}\s?\d{5}\s?\d{1,2}\s?\d/;
-    const nameRegex = /ชื่อตัวและชื่อสกุล\s+(.+)/;
-    const dobRegex = /เกิดวันที่\s+(\d+\s*\S+\s*\d+)/;
-    const addressRegex = /ที่อยู่\s+(.+)/;
-    const issueDateRegex = /วันออกบัตร\s+(\d+\s*\S+\s*\d+)/;
-    const expiryDateRegex = /วันบัตรหมดอายุ\s+(\d+\s*\S+\s*\d+)/;
+    const details = {
+      IdentificationNumber: text.match(/เลขประจำตัวประชาชน\s*(.*)/)[1],
+      Name: text.match(/ชื่อตัวและชื่อสกุล\s*(.*)/)[1],
+      DateOfBirth: text.match(/เกิดวันที่\s*(.*)/)[1],
+      Religion: text.match(/ศาสนา\s*(.*)/)[1],
+      Address: text.match(/ที่อยู่\s*(.*)/)[1],
+      DateOfIssue: text.match(/วันออกบัตร\s*(.*)/)[1],
+      IssuingOfficer: text.match(/เจ้าพนักงานออกบัตร\s*(.*)/)[1],
+      ExpiryDate: text.match(/วันบัตรหมดอายุ\s*(.*)/)[1],
+    };
 
-    const idMatch = text.match(idRegex);
-    const nameMatch = text.match(nameRegex);
-    const dobMatch = text.match(dobRegex);
-    const addressMatch = text.match(addressRegex);
-    const issueDateMatch = text.match(issueDateRegex);
-    const expiryDateMatch = text.match(expiryDateRegex);
+    // Clean up the data
+    details.IdentificationNumber = details.IdentificationNumber.trim();
+    details.Name = details.Name.trim();
+    details.DateOfBirth = details.DateOfBirth.trim();
+    details.Religion = details.Religion.trim();
+    details.Address = details.Address.trim();
+    details.DateOfIssue = details.DateOfIssue.trim();
+    details.IssuingOfficer = details.IssuingOfficer.trim();
+    details.ExpiryDate = details.ExpiryDate.trim();
 
-    // Display the extracted information
-    console.log("Identification Number:", idMatch ? idMatch[0] : "Not found");
-    console.log("Name:", nameMatch ? nameMatch[1].trim() : "Not found");
-    console.log("Date of Birth:", dobMatch ? dobMatch[1].trim() : "Not found");
-    console.log(
-      "Address:",
-      addressMatch ? addressMatch[1].trim() : "Not found"
-    );
-    console.log(
-      "Date of Issue:",
-      issueDateMatch ? issueDateMatch[1].trim() : "Not found"
-    );
-    console.log(
-      "Date of Expiry:",
-      expiryDateMatch ? expiryDateMatch[1].trim() : "Not found"
-    );
+    // Convert the date fields to ISO 8601 format
+    details.DateOfBirth = new Date(details.DateOfBirth).toISOString();
+    details.DateOfIssue = new Date(details.DateOfIssue).toISOString();
+    details.ExpiryDate = new Date(details.ExpiryDate).toISOString();
+
+    // Format the address field as a single string
+    details.Address = details.Address.replace(/\n/g, " ");
+
+    // Output the cleaned up data
+    console.log(details);
   })
   .catch((err) => {
     console.error("ERROR:", err);
