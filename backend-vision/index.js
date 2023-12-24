59f0ed58b674b7aab938c648c69ec061a2c01de3
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-24 18:55:06 +0530

    fix: api key secured

--- backend-vision/index.js
+++ backend-vision/index.js
@@ -1,10 +1,18 @@
 const vision = require("@google-cloud/vision");
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const app = express();
+// const app = express();
 
 const client = new vision.ImageAnnotatorClient({
-  keyFilename: "./APIKey.json",
+  keyFilename: "../../APIKey.json",
 });
+
+client
+  .textDetection("./abc.jpg")
+  .then((results) => {
+    // const text = results.fullTextAnnotation;
+
+    console.log(results[0].fullTextAnnotation.text);
+  })
+  .catch((err) => {
+    console.error("ERROR:", err);
+  });
