73a42ccfd4a31248c911a9dad234ad9ea36b17e9
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-24 18:15:28 +0530

    feat: google vision client initialization

--- backend-vision/index.js
+++ backend-vision/index.js
@@ -1,3 +1,10 @@
+const vision = require("@google-cloud/vision");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const app = express();
 
-
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: "./APIKey.json",
+});
