69fe3d9e92fa416da7f4fb47da558437517eb221
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-24 19:31:39 +0530

    feat: using geminiPro vision api, better results and takes use of OCR inherently

--- backend-vision/indexVertex.js
+++ backend-vision/indexVertex.js
@@ -1,1 +1,54 @@
 //trying something with vertex ai api
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs").promises;
+const dotenv = require("dotenv");
+dotenv.config();
+
+const api_key = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(api_key);
+const generationConfig = {
+  temperature: 0.4,
+  topP: 1,
+  topK: 32,
+  maxOutputTokens: 4096,
+};
+
+// Initialise Model for vision-based generation
+const model = genAI.getGenerativeModel({
+  model: "gemini-pro-vision",
+  generationConfig,
+});
+
+async function generateContent() {
+  try {
+    // Load image
+    const imagePath = "./aaa.jpg";
+    const imageData = await fs.readFile(imagePath);
+    const imageBase64 = imageData.toString("base64");
+
+    // Define parts
+    const parts = [
+      {
+        text: "Extract the following details - Identification Number, Name, Date of Birth, Religion, Address, Date of Issue, Issuing Officer, Expiry Date in JSON format in English Language:\n",
+      },
+      {
+        inlineData: {
+          mimeType: "image/jpeg",
+          data: imageBase64,
+        },
+      },
+    ];
+
+    // Generate content using both text and image input
+    const result = await model.generateContent({
+      contents: [{ role: "user", parts }],
+    });
+    const response = await result.response;
+    console.log(response.text());
+  } catch (error) {
+    console.error("Error generating content:", error);
+  }
+}
+
+// Run the function
+generateContent();
