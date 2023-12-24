9d0eb3376b30c340682724ab79fcc5d5124d9b6a
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-25 01:07:03 +0530

    feat: mongodb configuration

--- backend-vision/server.js
+++ backend-vision/server.js
@@ -3,13 +3,22 @@ const app = express();
 app.use(express.json());
 const { generateContent } = require("./indexVertex");
 
+//mongodb configuration
+
+const mongoUri =
+  "mongodb+srv://admin:admin@sajalcluster.ivqeyvz.mongodb.net/Thai_OCR?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri);
+
 //Endpoints to expose
 // 1. Get OCR Data based on date, id, identification number
 // 2. create a new OCR record with success/fail status
 // 3. update an exisiting OCR record
 // 4. delete an OCR record
 
-app.get("/record/:parameter", () => {});
+app.get("/records", async (req, res) => {
+  res.json({ records: await cards.find({}) });
+});
 app.post("/createrecord", () => {});
 app.put("/record/:id", () => {});
 app.delete("/record/:id", () => {});
