ea4eac3cf5f80256314735bf21c11ec34d366360
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-25 07:16:37 +0530

    feat: routing

---
+++ frontend-admin/src/components/Records.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditRecord from "./components/EditRecord";
+import Records from "./components/Records";
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#eeeeee",
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route path="/records" element={<Records />} />
+          <Route path="/record/:recordId" element={<EditRecord />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
