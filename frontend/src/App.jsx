6e68aeb15f6fb2b894a29107505687f7080b2bc3
Sajal Sharma <sajalsharma2408@gmail.com>
2023-12-25 10:58:07 +0530

    refactor: fetching details and displaying previous results

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,21 +10,54 @@ import {
   Button,
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import axios from "axios";
 
 function App() {
   const theme = useTheme();
-  const [, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
-  const [fetchedDetails, setFetchedDetails] = useState(null);
+  const [fetchedDetails, setFetchedDetails] = useState({});
   const [previousResults, setPreviousResults] = useState([]);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreviewImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const simulateServerRequest = async () => {
+    const formData = new FormData();
+    formData.append("file", selectedImage);
+    const response = await axios.post(
+      "http://localhost:3000/createrecord",
+      formData
+    );
+    const data = response.data.savedResponse;
+
+    // // Update fetched details
+    setFetchedDetails(data);
+    console.log(fetchedDetails);
+
+    // // Update previous results
+    setPreviousResults((prevResults) => [...prevResults, data]);
+  };
 
   return (
     <Grid container spacing={4} sx={{ p: 4 }}>
       <Grid item xs={12} md={8} sx={{ display: { xs: "block", md: "flex" } }}>
-        <Card sx={{ m: 2, borderRadius: 16, boxShadow: 1 }}>
+        <Card sx={{ m: 2, borderRadius: 8, boxShadow: 1 }}>
           <CardHeader
             title="Upload Image"
-            sx={{ p: 6, fontWeight: "bold", tracking: "tight" }}
+            sx={{
+              p: 6,
+              fontWeight: "bold",
+              tracking: "tight",
+              marginBottom: "-50px",
+            }}
           />
           <CardContent
             sx={{
@@ -42,7 +75,10 @@ function App() {
               sx={{ width: "100%", maxW: "sm" }}
             >
               <Grid item>
-                <InputLabel htmlFor="ocrImage" sx={{ fontSize: "0.875rem" }}>
+                <InputLabel
+                  htmlFor="ocrImage"
+                  sx={{ fontSize: "0.875rem", fontWeight: "bold" }}
+                >
                   Select an image
                 </InputLabel>
               </Grid>
@@ -65,7 +101,7 @@ function App() {
             {previewImage && (
               <img
                 src={previewImage}
-                style={{ marginTop: "1rem", maxWidth: "100%" }}
+                style={{ marginTop: "2rem", maxWidth: "80%" }}
               />
             )}
             <Button
@@ -80,6 +116,7 @@ function App() {
                 "&:hover": {
                   backgroundColor: theme.palette.primary.dark,
                 },
+                marginBottom: "-50px",
               }}
             >
               Process OCR
@@ -88,28 +125,57 @@ function App() {
         </Card>
       </Grid>
       <Grid item xs={12} md={4}>
-        <Card sx={{ m: 2, borderRadius: 16, boxShadow: 1 }}>
+        <Card sx={{ m: 2, borderRadius: 8, boxShadow: 1 }}>
           <CardHeader
             title="Fetched Details"
-            sx={{ p: 6, fontWeight: "bold", tracking: "tight" }}
+            sx={{
+              p: 4,
+              fontWeight: "bold",
+              tracking: "tight",
+              marginBottom: "-50px",
+            }}
           />
           <CardContent sx={{ p: 4 }}>
             {fetchedDetails ? (
-              <Typography>{fetchedDetails.details}</Typography>
+              <div>
+                <Typography>
+                  Identification Number: {fetchedDetails.identificationNumber}
+                </Typography>
+                <Typography>First Name: {fetchedDetails.firstName}</Typography>
+                <Typography>Last Name: {fetchedDetails.lastName}</Typography>
+                <Typography>Date of Birth: {fetchedDetails.dob}</Typography>
+                <Typography>Date of Issue: {fetchedDetails.doi}</Typography>
+                <Typography>Date of Expiry: {fetchedDetails.doe}</Typography>
+              </div>
             ) : (
               <Typography>No details fetched yet.</Typography>
             )}
           </CardContent>
         </Card>
-        <Card sx={{ m: 2, borderRadius: 16, boxShadow: 1 }}>
+        <Card sx={{ m: 2, borderRadius: 8, boxShadow: 1 }}>
           <CardHeader
             title="Previous Results"
-            sx={{ p: 6, fontWeight: "bold", tracking: "tight" }}
+            sx={{
+              p: 6,
+              fontWeight: "bold",
+              tracking: "tight",
+              marginBottom: "-50px",
+            }}
           />
-          <CardContent sx={{ p: 4 }}>
+          <CardContent sx={{ p: 4, marginBottom: "-50px" }}>
             {previousResults.length > 0 ? (
-              previousResults.map((result) => (
-                <Typography key={result.id}>{result.details}</Typography>
+              previousResults.map((result, index) => (
+                <div key={result._id} style={{ marginBottom: "50px" }}>
+                  <Typography>{index + 1}</Typography>
+                  <Typography>
+                    Identification Number: {result.identificationNumber}
+                  </Typography>
+                  <Typography>First Name: {result.firstName}</Typography>
+                  <Typography>Last Name: {result.lastName}</Typography>
+                  <Typography>Date of Birth: {result.dob}</Typography>
+                  <Typography>Date of Issue: {result.doi}</Typography>
+                  <Typography>Date of Expiry: {result.doe}</Typography>
+                </div>
               ))
             ) : (
               <Typography>No previous results.</Typography>
